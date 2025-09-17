7acd940de279ff1dd5fca7b2fdf43d8e678db394
dchege178-sudo <dchege178@gmail.com>
2025-09-17 21:39:32 +0300

    Add files via upload

---
+++ height,weight,phone number.c
@@ -0,0 +1,30 @@
+//simple height,weight and phone numder program
+
+/*
+Name:Danson Githuku Chege
+REG:CT100/G/26196/25
+Programe:Height,phone number and weight program
+*/
+
+#include <stdio.h>
+
+int main()
+{
+int height;
+
+   printf("enter height");
+   scanf("%d",&height);
+   printf("your height is%d\n",height);
+   
+int weight; 
+	printf("enter weight");
+	scanf("%d",&weight);
+	printf("your weight is%d\n",weight);
+	
+int phone;	
+		 
+   printf("enter phone number");
+   scanf("%d",&phone);
+   printf("your phone is%d\n",phone);
+   	return 0;
+}
