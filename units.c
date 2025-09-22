9cc8fb6519b0f15eccbe9de0b241153e8d460784
dchege178-sudo <dchege178@gmail.com>
2025-09-22 16:02:21 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,18 @@
+/*
+Name:Danson Githuku Chege
+REG:CT100/G/26196/25
+Description:Simple c programe for units for this semister project
+*/
+#include <stdio.h>
+
+int main(){
+	printf("sph 2101 Analogue electronics\n");
+	printf("spi 2101 Introduction to programing and aigorathm\n");
+	printf("spi 2102 Computer systems and organization\n");
+	printf("spi 2103 Introdction to computer application\n");
+	printf("spi 2120 Essentials of accounting\n");
+	printf("sph 2104 Mathematics for science\n");
+	printf("ucu 2105 Fundamentals of ICT\n");
+	printf("ucu 2106 Commmunication and information literacy\n");
+	return 0;
+}
